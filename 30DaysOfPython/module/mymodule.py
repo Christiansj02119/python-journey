1c8aa597021bae0e7104f311d2cfa82a4db3e627
christian san joaquin <111978789+Christiansj02119@users.noreply.github.com>
2026-07-29 15:21:05 +0800

    Update mymodule.py

--- 30DaysOfPython/module/mymodule.py
+++ 30DaysOfPython/module/mymodule.py
@@ -1,9 +1,21 @@
-
+import string
+from random import randint
 def generate_full_name(first_name, last_name):
     return first_name + ' ' + last_name
 def sumofnum1num2(num1, num2):
     return num1 + num2
-gravity = 10
+def random_user_id():
+    n = string.ascii_lowercase + string.digits
+    temp = []
+    result = ''
+    for item in range(0, 6):
+        temp.append(randint(0, len(n) - 1))
+    for item in temp:
+        result += n[item]
+        #print(n[item], end='')
+    return result
+
+gravity = 10    
 person = {
     'first_name': 'christian',
     'last_name': 'san joaquin',
