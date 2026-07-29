a07dac0c8c0787a70fb8bad096009036382ecef6
christian san joaquin <111978789+Christiansj02119@users.noreply.github.com>
2026-07-29 15:21:25 +0800

    Import random_user_id and print random character
    Added import for random_user_id and printed a random character.

--- 30DaysOfPython/module/main.py
+++ 30DaysOfPython/module/main.py
@@ -2,8 +2,8 @@ from mymodule import generate_full_name as full_name, sumofnum1num2 as sum, grav
 from math import pi, sqrt, pow, floor, ceil, log10
 import string
 from random import random, randint
+from mymodule import random_user_id as id
 import os
-import math
 print(os.listdir('30DaysOfPython'))
 if os.path.exists('30DaysOfPython/function_P.py'):
     print('file exists')
@@ -22,7 +22,7 @@ print(floor(6.67))
 print(ceil(6.67))
 print(log10(100))
 
-print(string.ascii_letters)
+print(string.ascii_letters + string.digits)
 print(string.digits)
 print(string.punctuation)
 print(ord('A'))
@@ -32,3 +32,10 @@ print(string.ascii_uppercase)
 print(string.ascii_letters)
 print(random()) # it doesn't take any arguments; it returns a value between 0 and 0.9999
 print(randint(1, 100)) # it returns a random integer number between [5, 20] inclusive
+
+n = string.ascii_lowercase + string.digits
+length = len(n)
+random = randint(0, length - 1)
+one_char = n[random]
+print(one_char)
+print(id())
