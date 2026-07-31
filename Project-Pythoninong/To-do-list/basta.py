e6be9f564e1e5da2049d28e7ef92f3494450a459
christian san joaquin <111978789+Christiansj02119@users.noreply.github.com>
2026-07-31 22:02:06 +0800

    Implement task management menu system
    Added a menu system for task management, allowing users to add, view, and remove tasks from the todo list.

--- Project-Pythoninong/To-do-list/basta.py
+++ Project-Pythoninong/To-do-list/basta.py
@@ -1,14 +1,40 @@
 import sqlite3
 conn = sqlite3.connect("todo_list.db")
 cursor = conn.cursor()
-
-
-
 with open("todo_list.sql", "r") as f:
     contents = f.read()
 
-cursor.execute(contents)
-cursor.execute(
-    "INSERT INTO todo_list (task, done, created_at, deadline) VALUES (?, ?, ?, ?)", 
-    ('buy milk', 0, '07/30/26', '08/05/26'))
 conn.commit()
+
+temp = True
+while temp:
+    print('MENU SYSTEM')
+    print('1. Add a new task')
+    print('2. View all task')
+    print('3. Remove a task')
+    print('4. Quit')
+    choice = input('Enter your choice:')
+    if choice == '1':
+        task = input('To do:')
+        if_done = int(input('input num 1 if done 0 if not done:'))
+        created_at = input('date created:')
+        deadline =  input('deadline:')
+        cursor.execute(contents)
+        cursor.execute("INSERT INTO todo_list(task, done, created_at, deadline) VALUES (?, ?, ?, ?)", (task, if_done, created_at, deadline))
+        conn.commit()
+    elif choice == '2':
+        cursor.execute("SELECT * FROM todo_list")
+        rows = cursor.fetchall()
+        print(rows)
+    elif choice == '3':
+        user_temp1 = input('Enter the IDS of the task to remove:')
+        temp1 = [int(item.strip()) for item in user_temp1.split(',')]
+        for item in temp1:
+            cursor.execute("DELETE FROM todo_list WHERE id = ?", (item,))
+
+        conn.commit()
+    elif choice == '4':
+        temp = False
+        print('tank you.')
+    else:
+        print('Invalid Input')
